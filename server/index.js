1498cd2e271483610267039bb15aa7b453b78839
ParadiseDM <ParadiseDMX@gmail.com>
2026-03-29 17:26:16 +0300

    Обновил файл

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import mongoose from 'mongoose';
 import multer from 'multer';
 import cors from 'cors';
@@ -21,6 +22,9 @@ const app = express();
 
 const storage = multer.diskStorage({
 	destination: (_, __, cb) => {
+		if (!fs.existsSync('uploads')) {
+			fs.mkdirSync('uploads');
+		}
 		cb(null, 'uploads'); 
 	},
 	filename: (_, file, cb) => {
