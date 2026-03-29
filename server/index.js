497ec12ec1e4d73bc32ee8998f7cc2103b419cab
ParadiseDM <ParadiseDMX@gmail.com>
2026-03-29 11:05:38 +0300

    first

---
+++ server/index.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import cors from 'cors';
+
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+
+import { UserController, PostController } from './controllers/index.js';
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+
+
+mongoose.connect(
+	'mongodb+srv://paradisedmx_db_user:gDCkRF0bf9OBzrAN@cluster0.yf7hjxq.mongodb.net/blog')
+	.then(() => console.log('DB ok'))
+	.catch(() => console.log('DB error', err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, 'uploads'); 
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname); 
+	},
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.originalname}`,
+	});
+});
+
+app.get('/tags', PostController.getLastTags);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+
+app.listen(4444, (err) => {
+	if (err) {
+		return console.log(err);
+	}
+	console.log('Server ОК')
+});
